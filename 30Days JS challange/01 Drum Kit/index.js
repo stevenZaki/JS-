c2728715242ac1cb18c54371276be5532e15b03b
unknown <stevensivo27@gmail.com>
2025-04-10 15:07:00 +0300

    Drum Kit

--- 30Days JS challange/01 Drum Kit/index.js
+++ 30Days JS challange/01 Drum Kit/index.js
@@ -1,6 +1,26 @@
 
-window.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}]"`);
+
+// Function to play the sounds. 
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+
+    if (!audio) return;
+
     audio.play();
-    if(!audio) return;
-});
+    audio.currentTime = 0;
+    key.classList.add('playing')
+}
+
+// Function to remove the transition. 
+function removeTransition(e){
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
+

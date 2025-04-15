f6b567bbed83058214fa834e4c0830da1aded308
unknown <stevensivo27@gmail.com>
2025-04-16 00:51:30 +0300

    Ajax Type Ahead

--- 30Days JS challange/06 Ajax/index.js
+++ 30Days JS challange/06 Ajax/index.js
@@ -4,6 +4,38 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 
 const cities = [];
 
-fetch(endpoint, function(){
+fetch(endpoint)
+        .then(blob => blob.json())
+        .then(data => cities.push(...data));
+
+
+function findMatches(wordToMatch, cities){
+    return cities.filter(place => {
+        
+        const regex = new RegExp(wordToMatch, 'ig');
+        return place.city.match(regex) || place.state.match(regex)
+    });
+}
+
+
+function displayMatches() {
     
-})
+    const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(place => {
+    
+        return `
+        <li>
+          <span class="name">${place.city}, ${place.state}</span>
+          <span class="population">${place.population}</span>
+        </li>
+      `;
+    }).join('');
+    suggestions.innerHTML = html;
+  }
+
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);

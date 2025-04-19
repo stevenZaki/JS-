5ef424d46e021ea3aec3d7f9df366694201c96fe
unknown <stevensivo27@gmail.com>
2025-04-19 17:35:16 +0300

    Hold Shift to Check Multiple Checkboxes

--- 30Days JS challange/10 Checkboxes/index.js
+++ 30Days JS challange/10 Checkboxes/index.js
@@ -3,16 +3,33 @@ const chceckbox = document.querySelectorAll('.inbox input[ type="checkbox"]');
 let lastChecked;
 
 function handleCheck(x){
-    let inBetween = false
-    
-    if(x.shiftkey && this.Checked){
+    let inBetween = false;
+
+    if(x.shiftkey && this.checked){
         chceckbox.forEach(chceckbox => {
             console.log(chceckbox);
         })
     }
 
     lastChecked = this;
-    // return;
 }
+
+
+
+
+// function handleCheck(e) {
+//     // Check if they had the shift key down
+//     // AND check that they are checking it
+//     let inBetween = false;
+//     if (e.shiftKey && this.checked) {
+//       // go ahead and do what we please
+//       // loop over every single checkbox
+//       checkboxes.forEach(checkbox => {
+//         console.log(checkbox);
+//     })
+//         }
+    
+//         lastChecked = this;
+//     }
 
 chceckbox.forEach(chceckbox => chceckbox.addEventListener('click', handleCheck))

5198694950ad3c2e86edf35ca2829013f35c21bc
unknown <stevensivo27@gmail.com>
2025-04-20 23:41:25 +0300

    Hold Shift to Check Multiple Checkboxes

--- 30Days JS challange/10 Checkboxes/index.js
+++ 30Days JS challange/10 Checkboxes/index.js
@@ -1,35 +1,28 @@
-const chceckbox = document.querySelectorAll('.inbox input[ type="checkbox"]');
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
 let lastChecked;
 
-function handleCheck(x){
-    let inBetween = false;
+function handleCheck(e) {
+  // Check if they had the shift key down
+  // AND check that they are checking it
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    // go ahead and do what we please
+    // loop over every single checkbox
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log('Starting to check them in between!');
+      }
 
-    if(x.shiftkey && this.checked){
-        chceckbox.forEach(chceckbox => {
-            console.log(chceckbox);
-        })
-    }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
 
-    lastChecked = this;
+  lastChecked = this;
 }
 
-
-
-
-// function handleCheck(e) {
-//     // Check if they had the shift key down
-//     // AND check that they are checking it
-//     let inBetween = false;
-//     if (e.shiftKey && this.checked) {
-//       // go ahead and do what we please
-//       // loop over every single checkbox
-//       checkboxes.forEach(checkbox => {
-//         console.log(checkbox);
-//     })
-//         }
-    
-//         lastChecked = this;
-//     }
-
-chceckbox.forEach(chceckbox => chceckbox.addEventListener('click', handleCheck))
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));

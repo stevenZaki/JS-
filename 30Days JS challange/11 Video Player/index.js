2193d353d8a28b460bb6926150ffd7143c0397bb
unknown <stevensivo27@gmail.com>
2025-04-22 00:18:17 +0300

    Custom HTML5 Video Player

--- 30Days JS challange/11 Video Player/index.js
+++ 30Days JS challange/11 Video Player/index.js
@@ -33,15 +33,36 @@ function skip(){
 
 function handleRangeUpdate() {
     video[this.name] = this.value;
-  }
+}
+
+
+function handleProgress(){
+    const percent = (video.currentTime / video.duration ) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(x){
+    const scrubTime = (x.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 
 // Add event listener 
 video.addEventListener('click', togglePlay);
 
 video.addEventListener('play', updateBotton);
 video.addEventListener('pause', updateBotton);
+video.addEventListener('timeupdate', handleProgress);
+
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+// progress.addEventListener('mousemove', (x) => mousedown && scrub(x));
+// progress.addEventListener('mousedown', () => mousedown = true);
+// progress.addEventListener('mouseup', () => mousedown = false);

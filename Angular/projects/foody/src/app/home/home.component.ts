e284d8beda3d014898961b237443702e7765d074
unknown <stevensivo27@gmail.com>
2025-05-01 22:46:32 +0300

    *

--- Angular/projects/foody/src/app/home/home.component.ts
+++ Angular/projects/foody/src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { FoodService } from '../services/food/food.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   foods:string[] = [];
   constructor (private foodService:FoodService){

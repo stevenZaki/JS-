d88195f9f06227b335900bff94ec94f46f279341
unknown <stevensivo27@gmail.com>
2025-05-13 00:52:28 +0300

    rout the search bar

--- Angular/projects/foody/src/app/app-routing.module.ts
+++ Angular/projects/foody/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent}
+  {path:'search/:searchTerm' , component:HomeComponent}
 ];
 
 @NgModule({
